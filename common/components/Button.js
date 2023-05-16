dcbc7e1684237d8f0e32f300bdbbe9ffed7a59d1
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-05-16 23:52:05 +0200

    feat - add reusable components

--- common/components/Button.js
+++ common/components/Button.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+// Material
+import MuiButton from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const Button = ({
+  type = "button",
+  variant = "contained",
+  isLoading = false,
+  disabled = false,
+  fullWidth = false,
+  compact = false,
+  onClick,
+  children,
+  sx,
+  ...rest
+}) => {
   return (
-    <div>Button</div>
-  )
-}
+    <MuiButton
+      type={type}
+      disabled={disabled || isLoading}
+      onClick={onClick}
+      fullWidth={fullWidth}
+      endIcon={isLoading && <CircularProgress size={20} />}
+      variant={variant}
+      sx={{
+        py: compact ? 0.5 : 1.5,
+        px: compact ? 2 : 4,
+        ...sx,
+      }}
+      {...rest}
+    >
+      {children}
+    </MuiButton>
+  );
+};
 
-export default Button
+export default Button;
