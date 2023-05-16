d18c8016b6c334c9f3ccb3606eb8c0944eb749a8
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-05-16 23:53:12 +0200

    feat - add new pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,46 @@
+// Mui
+import { ThemeProvider } from "@mui/material/styles";
+
+// Notistack
+import { SnackbarProvider } from "notistack";
+
 // Global Styles
-import { Global, css } from '@emotion/react'
-import CssBaseline from '@mui/material/CssBaseline';
+import { Global, css } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Theme
+import theme from "@/common/utils/theme";
 
 const GlobalStyle = css`
-    html {
-        scroll-behavior: smooth;
-    }
+  html {
+    scroll-behavior: smooth;
+  }
 
-    body {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
-        margin: 0;
-    }
+  body {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    margin: 0;
+  }
 
-    .SnackbarContent-root{
-        .SnackbarItem-message{
-            font-family: 'Poppins', sans-serif;
-            font-size: 14px;
-        }
-        .MuiSvgIcon-root{
-            color: white !important;
-        }
+  .SnackbarContent-root {
+    .SnackbarItem-message {
+      font-family: "Poppins", sans-serif;
+      font-size: 14px;
     }
-`
+    .MuiSvgIcon-root {
+      color: white !important;
+    }
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider>
+        <Global styles={GlobalStyle} />
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
